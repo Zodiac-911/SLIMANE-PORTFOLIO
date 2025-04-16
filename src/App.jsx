17671318b9a258b9ec4d9ece4d7e63033bb56d79
ZODIAC 911 <zodiacmadafac911@gmail.com>
2025-04-16 18:22:10 +0100

    browserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,19 @@ import Home from "./pages/home";
 import About from "./pages/about";
 import Portfolio from "./pages/portfolio";
 import Contact from "./pages/contact";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <WaveBackground />
       <TOPBAR />
-      <Contact />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/portfolio" element={<Portfolio />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
     </>
   );
 }
--- src/composant/topbar.jsx
+++ src/composant/topbar.jsx
@@ -1,12 +1,21 @@
 import "../styles/topbar.css";
+import { Link } from "react-router-dom";
 function TOPBAR() {
   return (
     <>
       <ul className="topbar-ul">
-        <li>Home</li>
-        <li>About</li>
-        <li>Portfolio</li>
-        <li>Contact</li>
+        <Link to="/">
+          <li>Home</li>
+        </Link>
+        <Link to="/about">
+          <li>About</li>
+        </Link>
+        <Link to="/portfolio">
+          <li>Portfolio</li>
+        </Link>
+        <Link to="/contact">
+          <li>Contact</li>
+        </Link>
       </ul>
     </>
   );
